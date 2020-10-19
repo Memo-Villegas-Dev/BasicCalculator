dd1c93fc0beb98fe43b2fadd5e043910a805e085
Guillermo Villegas <gvillegas@ucol.mx>
2020-10-19 15:35:53 -0500

    Nueva Calculadora cientifica

--- HelloWorld/app/src/main/java/com/androidavanzado/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/androidavanzado/helloworld/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button sumar, restar;
+    Button sumar, restar, limpiar, masdiez, menosdiez, signos;
     TextView texto;
     int num=0;
     @Override
@@ -18,9 +18,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         sumar = (Button) findViewById(R.id.btnsumar);
         restar = (Button) findViewById(R.id.btnrestar);
+        limpiar = (Button) findViewById(R.id.btnlimpiar);
+        masdiez = (Button) findViewById(R.id.btnmasdiez);
+        menosdiez = (Button) findViewById(R.id.btnmenosdiez);
+        signos = (Button) findViewById(R.id.btnsignos);
         texto = (TextView) findViewById(R.id.txt);
         sumar.setOnClickListener(this);
         restar.setOnClickListener(this);
+        limpiar.setOnClickListener(this);
+        masdiez.setOnClickListener(this);
+        menosdiez.setOnClickListener(this);
+        signos.setOnClickListener(this);
     }
 
     @Override
@@ -30,8 +38,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 num++;
                 texto.setText(num+"");
                 break;
+
             case R.id.btnrestar:
                 num--;
+                texto.setText(num+"");
+                break;
+
+            case R.id.btnlimpiar:
+                num = 0;
+                texto.setText(num+"");
+                break;
+
+            case R.id.btnmasdiez:
+                num = num + 10;
+                texto.setText(num+"");
+                break;
+
+            case R.id.btnmenosdiez:
+                num = num - 10;
+                texto.setText(num+"");
+                break;
+
+            case R.id.btnsignos:
+                num = num-(num*2);
                 texto.setText(num+"");
                 break;
         }
